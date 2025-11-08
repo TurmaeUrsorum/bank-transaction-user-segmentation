4047de2faae6c0de08ac03bb28e46a4d25e8e7cf
TurmaeUrsorum <projekbrillylutfan@gmail.com>
2025-11-08 19:17:21 +0700

    selesai juga pipeline nya

--- src/bank_transaction_user_segmentation/pipelines/evaluasi/pipeline.py
+++ src/bank_transaction_user_segmentation/pipelines/evaluasi/pipeline.py
@@ -3,8 +3,15 @@ This is a boilerplate pipeline 'evaluasi'
 generated using Kedro 1.0.0
 """
 
-from kedro.pipeline import Node, Pipeline  # noqa
-
+from kedro.pipeline import Node, Pipeline, node # noqa
+from .nodes import interpretasi_cluster
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    return Pipeline([
+        node(
+            func=interpretasi_cluster,
+            inputs=["modeling_kmedoids", "params:interpretasi_cluster"],
+            outputs="interpretasi_cluster",
+            name="interpretasi_cluster",
+        )
+    ])
